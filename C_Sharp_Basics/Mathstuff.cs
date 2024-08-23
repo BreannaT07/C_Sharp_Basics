656b64d6b556b81bd9d6ced51f8dcbb86bcffce3
breannatoth07 <breannatoth07@gmail.com>
2024-08-23 10:58:42 -0400

    Add project files.

---
+++ C_Sharp_Basics/Mathstuff.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basics
+{
+    internal class Mathstuff
+    {
+        public static void Math_Addition()
+        {
+            Console.WriteLine("Welcome to the greatest calulator ever");
+            Console.WriteLine("Lets try doing Addition and adding some numbers together.");
+            Console.Write("First, enter the 1st number to add: ");
+            double num_1 = double.Parse(Console.ReadLine());
+            Console.Write("Then, enter the 2nd number to add: ");
+            double num_2 = double.Parse(Console.ReadLine());
+            double the_sum = num_1 + num_2;
+            Console.WriteLine($"As {num_1} + {num_2} = {the_sum}");
+            Console.WriteLine($"The sum would be: {the_sum}");
+        }
+        public static void Math_Subtraction()
+        {
+            Console.WriteLine("With Addition out of the way, lets work on Subtraction.");
+            Console.Write("First, enter the 1st number to subtract: ");
+            double num_3 = double.Parse(Console.ReadLine());
+            Console.Write("Then, enter the 2nd number to subtract: ");
+            double num_4 = double.Parse(Console.ReadLine());
+            double the_sum = num_3 - num_4;
+            Console.WriteLine($"As {num_3} - {num_4} = {the_sum}");
+            Console.WriteLine($"The sum would be: {the_sum}");
+        }
+        public static void Math_Multiplication()
+        {
+            Console.WriteLine("As we wrap up Subtraction, lets go into Multplication.");
+            Console.Write("First, enter the 1st number to multiply: ");
+            double num_5 = double.Parse(Console.ReadLine());
+            Console.Write("Then, enter the 2nd number to multiply: ");
+            double num_6 = double.Parse(Console.ReadLine());
+            double the_sum = num_5 * num_6;
+            Console.WriteLine($"As {num_5} * {num_6} = {the_sum}");
+            Console.WriteLine($"The sum would be: {the_sum}");
+        }
+        public static void Math_Division()
+        {
+            Console.WriteLine("Now that we are done with Multplication, lets try division.");
+            Console.Write("First, enter the 1st number to divide: ");
+            double num_7 = double.Parse(Console.ReadLine());
+            Console.Write("Then, enter the 2nd number to divide: ");
+            double num_8 = double.Parse(Console.ReadLine());
+            double the_sum = num_7 / num_8;
+            Console.WriteLine($"As {num_7} / {num_8} = {the_sum}");
+            Console.WriteLine($"The sum would be: {the_sum}");
+        }
+
+        public static void Do_Stuff()
+        {
+            Console.WriteLine("_");
+        }
+    }
+}
---
+++ C_Sharp_Basics/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Comments();
+            //DataTypes();
+            //InputOutput();
+
+            //Mattstuff.cs V
+            Mathstuff.Math_Addition();
+            Mathstuff.Math_Subtraction();
+            Mathstuff.Math_Multiplication();
+            Mathstuff.Math_Division();
+            Mathstuff.Do_Stuff();
+
+
+
+            Console.ReadKey();
+        }
+
+        public static void Comments()
+        {
+            Console.WriteLine("Comments have slashes.");
+            // this is a comment
+            /* this
+             * is
+             * a
+             * multiline
+             * comment
+             */
+            Console.WriteLine("Multiline Commentysb start and end with /* */");
+
+        }
+        public static void DataTypes()
+        {
+            int myAge = 99;
+            Console.WriteLine(myAge);
+            char classGrade = 'A';
+            Console.WriteLine(classGrade);
+
+            float myFloat = 123.34F;
+            // or double myfloat = 123.34;
+            Console.WriteLine(myFloat);
+
+            decimal myDecimal = 86.7539M;
+            Console.WriteLine(myDecimal);
+
+            double myDouble = 123.34;
+            Console.WriteLine(myDouble);
+
+            bool myBool = true;
+            Console.WriteLine(myBool);
+
+            string myString = "Mr. Sekol is awesome :)";
+            Console.WriteLine(myString);
+        }
+        public static void InputOutput()
+        {
+            Console.Write("what is your name?: ");
+            string playername = Console.ReadLine();
+            Console.WriteLine($"Hello {playername}. How are you today? ");
+            // Readline takes in strings so if you wnat to do math, you have to cats the
+            // string indifferent datatype
+
+            //Let's math
+            Console.WriteLine("Welcome to my Addition calulator");
+            Console.Write("Please enter a number: ");
+            double num_1 = double.Parse(Console.ReadLine());
+            Console.Write("Please enter a 2nd number: ");
+            double num_2 = double.Parse(Console.ReadLine());
+            double the_sum = num_1 + num_2;
+            Console.WriteLine($"{num_1} + {num_2} = {the_sum}");
+
+            Console.WriteLine("Let's do multiplication");
+            Console.Write("Please enter a number: ");
+            double num_3 = double.Parse(Console.ReadLine());
+            Console.Write("Please enter a 2nd number: ");
+            double num_4 = double.Parse(Console.ReadLine());
+            double the_product = num_3 * num_4;
+            Console.WriteLine($"{num_3} x {num_4} = {the_product}");
+        }
+
+    }
+}
